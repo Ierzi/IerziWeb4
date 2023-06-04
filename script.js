ea4f3128dfd5014578e665a4e1996fa94acce321
Ierzi <105780029+Ierzi@users.noreply.github.com>
2023-06-04 19:56:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,21 @@
 
 // Action du bouton Projects. Voir menuindex.html
 function projects_clicked() {
-    console.log("Projects Button Clicked") 
+    console.log("Projects Button Clicked");
 }
 
 // Action du bouton Source Code. Voir menuindex.html
 function sourcecode_clicked() {
-    console.log("Source Code Button Clicked")
-    window.open("https://github.com/Ierzi/IerziWeb4", "_blank")
+    console.log("Source Code Button Clicked");
+    console.log("Opening a new tab on the window...");
+    window.open("https://github.com/Ierzi/IerziWeb4", "_blank");
+    console.log("Done.");
 
+}
+
+function news_clicked() {
+    console.log("News Button CLicked");
+    console.log("Openinga new tab on the window...");
+    window.location.href = "newsindex.html";
+    console.log("Done.");
 }
