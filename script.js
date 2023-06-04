5f346c95029c275fb21d29662fbd6cd3635a886b
Ierzi <105780029+Ierzi@users.noreply.github.com>
2023-06-04 19:43:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function b1_is_clicked() {
-    console.log("Button is clicked")
+// Création des fontions
+
+// Action du bouton Projects. Voir menuindex.html
+function projects_clicked() {
+    console.log("Projects Button Clicked") 
+}
+
+// Action du bouton Source Code. Voir menuindex.html
+function sourcecode_clicked() {
+    console.log("Source Code Button Clicked")
+    window.open("https://github.com/Ierzi/IerziWeb4", "_blank")
+
 }
